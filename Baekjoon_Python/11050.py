107cf3e74a0a1317204c9454863c33a5ebbe3c6f
YuLim2 <jyulim0120@gmail.com>
2022-06-20 01:06:30 +0900

    11050

---
+++ Baekjoon_Python/11050.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+def fac(n):
+    if n == 0:
+        return 1
+    return n*fac(n-1)
+
+n, k = map(int, input().split())
+print(fac(n) // (fac(k) * fac(n-k)))
