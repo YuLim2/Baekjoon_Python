d96b3b41b4217b3c75bbb82ecf4675edc9f31301
YuLim2 <jyulim0120@gmail.com>
2022-06-20 00:09:55 +0900

    2098

--- Baekjoon_Python/2098.py
+++ Baekjoon_Python/2098.py
@@ -1,24 +1,27 @@
 import sys
-input = sys.stdin.readline
 
-n = int(input())
+N = int(sys.stdin.readline())
+W = []
 INF = sys.maxsize
-dp = [[INF]*(1 << n) for _ in range(n)]
-graph = []
+for _ in range(N):
+    W.append(list(map(int, sys.stdin.readline().split())))
+DP = [[None] * (1 << N) for _ in range(N)]
 
-def dfs(x, visited): # 현재위치
-    if visited == (1 << n) - 1:  # 모두 방문햇다면
-        return graph[x][0] or INF  # 0으로 가는방법 반환 없으면 INF반환
 
-    if dp[x][visited] is not None:  # 이미 계산되어잇다면
-        return dp[x][visited]  # 있는값 반환
+def find_path(last, visited):  # 현재위치
+    if visited == (1 << N) - 1:  # 모두 방문햇다면
+        return W[last][0] or INF  # 0으로 가는방법 반환 없으면 INF반환
+
+    if DP[last][visited] is not None:  # 이미 계산되어잇다면
+        return DP[last][visited]  # 있는값 반환
 
     tmp = INF
-    for city in range(n):  # 모든 도시에서
-        if visited & (1 << city) == 0 and graph[x][city] != 0:  # 아직 방문하지 않았고 cur->i로 가는길이 있다면
-            tmp = min(tmp, dfs(city, visited | (1 << city)) + graph[x][city])
-    dp[x][visited] = tmp
+    for city in range(N):  # 모든 도시에서
+        if visited & (1 << city) == 0 and W[last][city] != 0:  # 아직 방문하지 않았고 cur->i로 가는길이 있다면
+            tmp = min(tmp,
+                      find_path(city, visited | (1 << city)) + W[last][city])
+    DP[last][visited] = tmp
     return tmp
 
 
-print(dfs(0, 1 << 0))
+print(find_path(0, 1 << 0))
