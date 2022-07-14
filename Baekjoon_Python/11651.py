2e8f25843494f079c878773899dd179dedb5b7cf
YuLim2 <jyulim0120@gmail.com>
2022-07-15 08:50:34 +0900

    11651

---
+++ Baekjoon_Python/11651.py
@@ -0,0 +1,10 @@
+n = int(input())
+ls = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    ls.append((x, y))
+
+ls.sort(key=lambda x: (x[1], x[0]))
+
+for i in ls:
+    print(i[0], i[1])
