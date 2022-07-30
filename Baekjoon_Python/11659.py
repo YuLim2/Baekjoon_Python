8a33ef17e2a9d63d3b2f40021ebcb65f94b87f04
YuLim2 <jyulim0120@gmail.com>
2022-07-30 16:26:54 +0900

    11659

--- Baekjoon_Python/11659.py
+++ Baekjoon_Python/11659.py
@@ -4,6 +4,10 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 ls = list(map(int, input().split()))
 
+for i in range(n-1):
+    ls[i+1] += ls[i]
+ls = [0] + ls
+
 for _ in range(m):
-    a, b = map(int, input().split())
-    print(sum(ls[a-1:b]))
+    a,b = map(int,input().split())
+    print(ls[b]-ls[a-1])
