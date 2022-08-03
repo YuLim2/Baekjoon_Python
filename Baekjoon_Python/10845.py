d0c50d411584bb5d3f417567e3fde9c354dd8d5b
YuLim2 <jyulim0120@gmail.com>
2022-08-03 15:10:10 +0900

    10845

---
+++ Baekjoon_Python/10845.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+
+for _ in range(n):
+    cmd = input().split()
+    if cmd[0] == 'push':
+        q.append(cmd[1])
+    elif cmd[0] == 'front':
+        if len(q) == 0: print(-1)
+        else: print(q[0])
+    elif cmd[0] == 'back':
+        if len(q) == 0: print(-1)
+        else: print(q[-1])
+    elif cmd[0] == 'size':
+        print(len(q))
+    elif cmd[0] == 'pop':
+        if q: print(q.pop(0))
+        else: print(-1)
+    elif cmd[0] == 'empty':
+        if len(q) == 0: print(1)
+        else: print(0)
