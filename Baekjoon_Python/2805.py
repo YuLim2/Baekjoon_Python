3173a9738307cf537aab09e30b7ab4c2405f2618
YuLim2 <jyulim0120@gmail.com>
2022-08-05 22:04:07 +0900

    2805

---
+++ Baekjoon_Python/2805.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+ls = map(int, input().split())
+
+start, end = 0, max(ls)
+
+# 이분 탐색
+while start <= end:
+    mid = (start+end)//2
+    tree = 0
+    for i in ls:
+        if i > mid:
+            tree += i - mid
+    if tree >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
