7f665ccbea924aa58348b60b9babb37f76943a0c
YuLim2 <jyulim0120@gmail.com>
2022-08-12 22:14:23 +0900

    1193

---
+++ Baekjoon_Python/1193.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().split())
+ls = list(map(int, input().split()))
+
+ls2 = sorted(list(set(ls)))
+dic = {ls2[i] : i for i in range(len(ls2))}
+for i in ls:
+    print(dic[i], end = ' ')
