9a37e974cf94106fa8a9f1d5e6ac2ad590837e0a
YuLim2 <jyulim0120@gmail.com>
2022-08-16 09:06:46 +0900

    2579

---
+++ Baekjoon_Python/2579.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+ls = [int(input()) for _ in range(n)]
+dp = []
+
+dp[0], dp[1], dp[2] = ls[0], ls[0]+ls[1], max(ls[1]+ls[2], ls[0]+ls[2])
+for i in range(3, n):
+    dp[i] = max(dp[i-3] + ls[i-1] + ls[i], dp[i-2] + ls[i])
+
+print(dp[n - 1])
