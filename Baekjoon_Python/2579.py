d225f341406802ac689b59ef3059d91cb5a25a40
YuLim2 <jyulim0120@gmail.com>
2022-08-16 09:12:13 +0900

    2579
    out of range - make space of dp list

--- Baekjoon_Python/2579.py
+++ Baekjoon_Python/2579.py
@@ -3,9 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 ls = [int(input()) for _ in range(n)]
-dp = []
+dp = [0]*n
 
-dp[0], dp[1], dp[2] = ls[0], ls[0]+ls[1], max(ls[1]+ls[2], ls[0]+ls[2])
+dp[0] = ls[0]
+dp[1] = ls[0]+ls[1]
+dp[2] = max(ls[1]+ls[2], ls[0]+ls[2])
+
 for i in range(3, n):
     dp[i] = max(dp[i-3] + ls[i-1] + ls[i], dp[i-2] + ls[i])
 
